1058afcd16e7f977fc94d60372712646510e3365
mallina <you@mallina.os>
2025-11-17 15:39:12 +0300

    added classes and tests

---
+++ src/main/java/com/mipt/olgamallina/DataService.java
@@ -0,0 +1,11 @@
+package com.mipt.olgamallina;
+
+import java.util.Optional;
+
+public interface DataService {
+    Optional<String> findDataByKey(String key);
+
+    void saveData(String key, String data);
+
+    boolean deleteData(String key);
+}

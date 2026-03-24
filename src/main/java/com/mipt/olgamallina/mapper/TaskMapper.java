a8007f263abc1a7d0961bc69e6041d3210135b09
mallina <you@mallina.os>
2026-03-24 21:12:17 +0300

    Add in-memory repositories for tasks and attachments

---
+++ src/main/java/com/mipt/olgamallina/mapper/TaskMapper.java
@@ -0,0 +1,27 @@
+package com.mipt.olgamallina.mapper;
+
+import com.mipt.olgamallina.dto.TaskCreateDto;
+import com.mipt.olgamallina.dto.TaskResponseDto;
+import com.mipt.olgamallina.dto.TaskUpdateDto;
+import com.mipt.olgamallina.model.Task;
+import org.mapstruct.BeanMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+@Mapper(componentModel = "spring")
+public interface TaskMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "completed", constant = "false")
+    @Mapping(target = "createdAt", ignore = true)
+    Task toEntity(TaskCreateDto dto);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    void updateEntity(TaskUpdateDto dto, @MappingTarget Task task);
+
+    TaskResponseDto toResponseDto(Task task);
+}
